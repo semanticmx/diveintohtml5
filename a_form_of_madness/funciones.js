afc6679dde7c1ad14256f605f31c5aa501ff4eb6
Carmen-Mercado <carmen@semantic.mx>
2020-10-27 18:26:34 -0600

    Ejemplos de capitulos

---
+++ a_form_of_madness/funciones.js
@@ -0,0 +1,23 @@
+
+  function auto()
+    {
+        alert("ee");
+        if (!("autofocus" in document.createElement("input"))==false) {
+            alert("3")
+           document.getElementById("nombre").focus();
+          }
+        
+    }
+window.onload=auto;
+function muestra()
+{
+    var s=document.getElementById("nombre").value;
+    s=s+"\n"+document.getElementById("edad").value;
+    s=s+"\n"+document.getElementById("fecha").value;
+    s=s+"\n"+document.getElementById("email").value;
+    s=s+"\n"+document.getElementById("color").value;
+    s=s+"\n"+document.getElementById("opciones").value;
+    
+    alert(s);
+
+}
