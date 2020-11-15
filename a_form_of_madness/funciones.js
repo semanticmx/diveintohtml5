4cdb7d58892b17be0c8ddaea2df053c5d227f529
carmen514 <carmen24597mr@gmail.com>
2020-11-15 14:47:28 -0600

    Revert "Revert "Revert "Revert "Correcciones de los archivos""""
    This reverts commit 4240c0ec81f36f11b6b0b3a4e092f8e0716569b3.

--- a_form_of_madness/funciones.js
+++ a_form_of_madness/funciones.js
@@ -1,23 +1,29 @@
-
-  function auto()
-    {
-       
-        if (!("autofocus" in document.createElement("input"))==false) {
-            
+  function auto(){
+    document.getElementById("enviar").addEventListener("click",muestra);
+    if (!("autofocus" in document.createElement("input"))==false) {        
            document.getElementById("nombre").focus();
-          }
-        
-    }
+          }   
+  }
 window.onload=auto;
-function muestra()
+function muestra(e)
 {
-    var s=document.getElementById("nombre").value;
-    s=s+"\n"+document.getElementById("edad").value;
-    s=s+"\n"+document.getElementById("fecha").value;
-    s=s+"\n"+document.getElementById("email").value;
-    s=s+"\n"+document.getElementById("color").value;
-    s=s+"\n"+document.getElementById("opciones").value;
-    
-    alert(s);
-
+    var s="<h4>Los datos ingresados anteriormente son: </h4>";
+    s+=document.getElementById("nombre").value+"<br>";
+    s+="\n"+document.getElementById("edad").value+"<br>";
+    s+="\n"+document.getElementById("fecha").value+"<br>";
+    s+="\n"+document.getElementById("email").value+"<br>";
+    s+="\n"+document.getElementById("color").value+"<br>";
+    s+="\n"+document.getElementById("opciones").value+"<br>";
+    document.getElementById("datos").innerHTML=s;
+    e.preventDefault();
+    limpia();
 }
+function limpia()
+{
+  document.getElementById("nombre").value="";
+  document.getElementById("edad").value="";
+  document.getElementById("fecha").value="";
+  document.getElementById("email").value="";
+  document.getElementById("opciones").value="";
+}
+
